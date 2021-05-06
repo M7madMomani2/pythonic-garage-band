9d79e688cde4d32d95d620ba54da62bdc29c80fc
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-06 03:13:28 +0300

    finally lab solutio/mnt/c/Users/PC/ltuc/pythonic-garage-band/.venv/bin/python /mnt/c/Users/PC/ltuc/pythonic-garage-band/pythonic_garage_band/pythonic_garage.pyn

--- pythonic_garage_band/pythonic_garage.py
+++ pythonic_garage_band/pythonic_garage.py
@@ -1,68 +1,70 @@
-class Musician :
-    def __init__(self,members):
-        self.members=members
-    def __str__ (self):
-        print("------------")
-
-    def __repr__(self):
-        print("------------")
-
-    def get_instrument(self):
-        return "method that returns string."
-
-    def play_solo(self):
-        return "method that returns string."
-
-
-
-class Band(Musician) :
+class Band:
     def __init__(self,name,members):
-        self.name = name 
-        super().__init__(members)
-
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
+    
     def play_solos(self):
-        print("method <>-play a solo")
-
+        message=[]
+        for i in self.members:
+            string_message=str(i).split()[len(str(i).split())-1::]
+            # print(string_message[0])
+            if string_message[0] == 'guitar':
+                message.append('face melting guitar solo')
+            if string_message[0] == 'bass':
+                message.append('bom bom buh bom')
+            if string_message[0] == 'drums':
+                message.append('rattle boom crash')
+        return message
         
     def __str__(self):
-        return f"My name is {self.name} and I play {self.get_instrument()}"
-
-    def __repr__ (self):
-        print("-----------")
-
+        return f"The band {self.name} has the following members {self.members}"
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__} instance. Name = {self.name}"
+    
     @classmethod
     def to_list(cls):
-        print(cls.members)
+        return cls.instances
 
 
-class Guitarist(Band) :
-    def __init__(self,name,members):
-        self.name = name 
+
+class Musician():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return f"{self.instrument}"
+
+    def play_solo(self):
+        return f"{self.solo_play}"
+
+
+
+class Guitarist(Musician):
+    instrument = 'guitar'
+    def __init__(self, name):
         super().__init__(name)
+        self.solo_play = 'face melting guitar solo'
 
-    def get_instrument():
-        return "guitar"
 
-    def play_solos(self):
-        print("face melting guitar solo")
 
-class Bassist(Band) :
-    def __init__(self,name,members):
-        self.name = name 
+class Bassist(Musician):
+    instrument = 'bass'
+    def __init__(self, name):
         super().__init__(name)
+        self.solo_play = 'bom bom buh bom'
 
-    def get_instrument():
-        return "bass"
-    def play_solos(self):
-        print("bom bom buh bom")
 
-class Drummer(Band) :
-    def __init__(self,name,members):
-        self.name = name 
+class Drummer(Musician):
+    instrument = 'drums'
+    def __init__(self, name):
         super().__init__(name)
-
-    def get_instrument():
-        return "drums"
-    def play_solos(self):
-        print("rattle boom crash")
+        self.solo_play = 'rattle boom crash'
 
